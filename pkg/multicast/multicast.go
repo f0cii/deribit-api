382eaec00b95c2e4d1e1f0877673171c15514226
Haiss2 <hai98th@gmail.com>
2022-07-27 11:43:30 +0700

    fixbug/emit_pointer_instead_of_value_for_multicast

--- pkg/multicast/multicast.go
+++ pkg/multicast/multicast.go
@@ -449,12 +449,12 @@ func (c *Client) Handle(m *sbe.SbeGoMarshaller, r io.Reader) error {
 				tradeIns := trades[0].InstrumentName
 				tradeKind := trades[0].InstrumentKind
 				currency := getCurrencyFromInstrument(tradeIns)
-				c.Emit(newTradesNotificationChannel(tradeKind, currency), event.Data)
+				c.Emit(newTradesNotificationChannel(tradeKind, currency), &event.Data)
 			}
 
 		case EventTypeTicker:
 			ticker := event.Data.(models.TickerNotification)
-			c.Emit(newTickerNotificationChannel(ticker.InstrumentName), event.Data)
+			c.Emit(newTickerNotificationChannel(ticker.InstrumentName), &event.Data)
 		}
 	}
 
